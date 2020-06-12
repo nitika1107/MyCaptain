8755a1d412df67a3404903360c4d5664079b14c9
nitika1107 <64460667+nitika1107@users.noreply.github.com>
2020-06-12 12:34:53 +0530

    created assignment.py

---
+++ assignment3.py
@@ -0,0 +1,12 @@
+num1=[5,10,15,20]
+print(num1,"is the list")
+num1.append(25)
+print(num1,"is the new list")
+mytuple=(2,4,6,8)
+print(mytuple,"is a tuple")
+print(len(mytuple),"is the tuple length")
+print(mytuple[2],"is the third element of tuple")
+mydict = {1: 'My', 2: 'Captian', 3: 'App'} 
+print(mydict)
+del mydict[3]
+print(mydict)
